fa5593fd09cfa33a581c56c117c79d73520cdfa6
Jordan Jones <jordpjones@gmail.com>
2021-03-20 00:22:30 -0700

    push

--- config.js
+++ config.js
@@ -1,9 +1,12 @@
 module.exports = {
-    port: 3000,
-    bandwidth: {
-        accountId: '',
-        token: '',
-        key: '',
-        url:
-    }
-}
+	port: 3000,
+	bandwidth: {
+		accountId: '',
+		token: '',
+		key: '',
+		url: ''
+	},
+	sendgrid: {
+		key: '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'
+	}
+};
--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+
 const Bandwidth = require('@bandwidth/messaging');
+const Sendgrid = require('@sendgrid/mail');
+
+const config = require('../config');
+
+Sendgrid.setApiKey(config.sendgrid.key);
 
 const app = express();
 
@@ -10,8 +16,23 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.get('/', (req, res) => res.redirect('https://www.youtube.com/watch?v=dQw4w9WgXcQ'));
 
 app.post('/sendgrid/incoming', async (req, res) => {
-    console.log(req.body);
-    return res.status(204);
-})
+	console.log(req);
+	return res.status(204);
+});
+
+app.post('/bandwidth/incoming', async (req, res) => {
+	const msg = {
+		to: 'jordpjones@gmail.com',
+		from: 'jordpjones@gmail.com',
+		subject: 'Sending with Sendgird',
+		text: 'Its easy',
+		html: '<strong>test</strong>'
+	};
+
+	Sendgrid.send(msg)
+		.then(() => console.log('Email sent'))
+		.catch((error) => console.error(error));
+	return res.status(204);
+});
 
 app.listen(3000, () => console.log('Server is listening on 3000'));
