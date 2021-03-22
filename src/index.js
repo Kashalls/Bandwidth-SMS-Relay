a45c29d72ba7e0755e891adccc3a5246f03fcad8
Jordan Jones <jordpjones@gmail.com>
2021-03-21 19:48:16 -0700

    Use multer

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const morgan = require('morgan');
-const bodyParser = require('body-parser');
+const multer = require('multer');
 
 const Bandwidth = require('@bandwidth/messaging');
 const Sendgrid = require('@sendgrid/mail');
@@ -12,12 +12,13 @@ Sendgrid.setApiKey(config.sendgrid.key);
 const app = express();
 
 app.use(morgan('combined'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json({ limit: '10mb' }));
+
+const upload = multer();
 
 app.get('/', (req, res) => res.redirect('https://www.youtube.com/watch?v=dQw4w9WgXcQ'));
 
-app.post('/sendgrid/incoming', async (req, res) => {
+app.post('/sendgrid/incoming', upload.none(), async (req, res) => {
 	console.log(req);
 	return res.status(204).send();
 });
